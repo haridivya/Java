14662401eefc157be8e0a95772b39ff11f6c6fcc
haridivya <123063502+haridivya@users.noreply.github.com>
2026-07-17 23:13:06 +0530

    TwoPointers
    Implement merge function to combine two sorted arrays.

---
+++ MergeSortedArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int i=0;
+      int j=0;
+      while(j<n)
+      {
+        if(nums1[i]==0){
+            nums1[i]=nums2[j];
+            j++;
+        }
+        i++;
+      } 
+      Arrays.sort(nums1);
+      System.out.println(nums1); 
+    }
+}
